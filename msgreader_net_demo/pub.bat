f078ce94558d52e66b97b75d649263d02853dcfb
kenjiuno <ku@digitaldolphins.jp>
2023-10-17 22:12:33 +0900

    pub.bat

--- msgreader_net_demo/pub.bat
+++ msgreader_net_demo/pub.bat
@@ -1,1 +1,1 @@
-dotnet publish -c:Release -p:GHPages=true -p:GHPagesBase=/msgreader_net_demo && robocopy /mir publish\wwwroot ..\docs
+dotnet publish -o publish -c:Release -p:GHPages=true -p:GHPagesBase=/msgreader_net_demo/ && robocopy /e publish\wwwroot ..\docs
